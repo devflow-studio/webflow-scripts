cb9d220888bc3326ab4c584551981be237689a36
PotterSource <davidpotterstudio@gmail.com>
2025-01-24 00:20:07 -0600

    configure rollup for dist files

--- scripts/webflow-scripts/src/hello-world/index.test.ts
+++ scripts/webflow-scripts/src/hello-world/index.test.ts
@@ -1,8 +1,8 @@
 describe('Hello World', () => {
   // listen to console.log - output should be 'Hello World'
-  it('should log "Hello World"', async () => {
+  it('should log "Hello, world!"', async () => {
     const logSpy = jest.spyOn(console, 'log').mockImplementation()
-    await import('./index.ts')
+    await import('./index')
     expect(logSpy).toHaveBeenCalledWith('Hello, world!')
     logSpy.mockRestore()
   })
