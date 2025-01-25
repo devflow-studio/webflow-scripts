f5518918026d0373812cff663c85c046043529e3
PotterSource <davidpotterstudio@gmail.com>
2025-01-24 22:42:16 -0600

    add build entrypoint

--- scripts/webflow-scripts/rollup.config.mjs
+++ scripts/webflow-scripts/rollup.config.mjs
@@ -1,22 +1,45 @@
 import typescript from '@rollup/plugin-typescript'
 import terser from '@rollup/plugin-terser'
 
-export default {
-  input: {
-    'hello-world': 'src/hello-world/index.ts',
+export default [
+  {
+    input: {
+      'hello-world': 'src/hello-world/index.ts',
+    },
+    output: {
+      dir: 'dist',
+      entryFileNames: '[name]/index.js',
+      name: 'HelloWorld',
+      format: 'iife',
+      sourcemap: true,
+      preserveModules: false,
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/*.test.ts'],
+      }),
+      terser(),
+    ],
   },
-  output: {
-    dir: 'dist',
-    entryFileNames: '[name]/index.js',
-    format: 'iife',
-    sourcemap: true,
-    preserveModules: false,
+  {
+    input: {
+      'hubspot-submit': 'src/hubspot-submit/index.ts',
+    },
+    output: {
+      dir: 'dist',
+      entryFileNames: '[name]/index.js',
+      name: 'HubspotSubmit',
+      format: 'iife',
+      sourcemap: true,
+      preserveModules: false,
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ['**/*.test.ts'],
+      }),
+      terser(),
+    ],
   },
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json',
-      exclude: ['**/*.test.ts'],
-    }),
-    terser(),
-  ],
-}
+]
